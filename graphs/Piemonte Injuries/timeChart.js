434076e97793df6cf32d8c2ed4de0f2c3110bf0f
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-31 17:50:42 -0400

    formatting date data

--- graphs/Piemonte Injuries/timeChart.js
+++ graphs/Piemonte Injuries/timeChart.js
@@ -1,6 +1,28 @@
 async function drawTimeChart(){
-    const calendar = new CalHeatmap();
-    calendar.paint({});
+    const calendar2024 = new CalHeatmap();
+    const calendar2025 = new CalHeatmap();
+
+    calendar2024.paint({
+        range: 3,
+        domain: { type: 'month' },
+        subDomain: {
+            type: 'day',
+        },
+        date:{
+            start: new Date('2024-01-01')
+        }
+    });
+
+    calendar2025.paint({
+        range: 3,
+        domain: { type: 'month' },
+        subDomain: {
+            type: 'day',
+        },
+        date:{
+            start: new Date('2025-01-01')
+        }
+    });
 }
 
 export { drawTimeChart }
